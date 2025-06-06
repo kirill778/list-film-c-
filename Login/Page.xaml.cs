62ab4c92f1fab5f9062a2d566a58e1b11be6a82e
Your <k.romanov@corp.sg.local>
2025-06-06 17:55:47 +0300

    first

---
+++ Login/Page.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace FilmsListWPF.Login
+{
+    /// <summary>
+    /// Interaction logic for Page.xaml
+    /// </summary>
+    public partial class Page : UserControl
+    {
+        public Page()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e){
+            string email = EmailTextBox.Text;
+            string password = PasswordBox.Password;
+
+            checkUser(email, password);
+        }
+        
+        private void checkUser(string email, string password){
+            if (email == "admin" && password == "1234"){
+                MessageBox.Show("Login successful!");
+            }
+            else{
+                MessageBox.Show("Invalid email or password.");
+            }
+        }
+    }
+}
